c891ad1addf502f46cbb2f2e472ca18181b8d6ac
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-25 16:19:02 +0900

    fixed tss

--- test/auth.js
+++ test/auth.js
@@ -1,44 +1,35 @@
 const request = require("supertest")
-const {expect} = require("chai")
-const {app} = require("../index.js")
+const { expect } = require("chai")
+const { app } = require("../index.js")
 
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
-const sleep = (delay) => new Promise(resolve => setTimeout(resolve,delay))
-
-const {
-  TEST_USERNAME = 'admin',
-  TEST_PASSWORD = 'admin',
-} = process.env
-
+const { TEST_USERNAME = "admin", TEST_PASSWORD = "admin" } = process.env
 
 // We will test for api users
-describe("/v2/auth", () => {
-
-  before( async () => {
+describe("/auth", () => {
+  before(async () => {
     //console.log = function () {}
     await sleep(8000) // wait for admin to be created
   })
 
-
   // We will test root GET related logics
-  describe("POST /v2/auth/login", () => {
-
+  describe("POST /auth/login", () => {
     // What should it do
     it("Should allow admin login", async () => {
-      const {status} = await request(app)
-        .post("/v2/auth/login")
-        .send({username: TEST_USERNAME, password: TEST_PASSWORD})
+      const { status } = await request(app)
+        .post("/auth/login")
+        .send({ username: TEST_USERNAME, password: TEST_PASSWORD })
 
       expect(status).to.equal(200)
     })
 
     it("Should not allow random user login", async () => {
-      const {status} = await request(app)
-        .post("/v2/auth/login")
-        .send({username: 'roger', password: 'banana'})
+      const { status } = await request(app)
+        .post("/auth/login")
+        .send({ username: "roger", password: "banana" })
 
       expect(status).to.not.equal(200)
     })
   })
-
 })
