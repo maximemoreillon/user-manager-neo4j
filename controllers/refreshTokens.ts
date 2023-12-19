6816dded21435a293f607ec9589fbe9e58d8464f
Maxime Moreillon <m.moreillon@gmail.com>
2023-12-20 08:26:41 +0900

    removed refresh token logic for now

--- controllers/refreshTokens.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express"
-
-export const refreshAccessToken = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  res.status(501).send("Not implemented")
-}
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,7 +1,6 @@
 import express from "express"
 import { login } from "../controllers/auth"
 import { request_password_reset } from "../controllers/password"
-import { refreshAccessToken } from "../controllers/refreshTokens"
 import { decodeAccessToken } from "../controllers/accessTokens"
 
 const router = express.Router()
@@ -9,7 +8,6 @@ const router = express.Router()
 router.route("/login").post(login)
 router.route("/token").post(decodeAccessToken)
 router.route("/access-token").post(decodeAccessToken)
-router.route("/refresh-token").post(refreshAccessToken)
 router.route("/password/reset").post(request_password_reset)
 
 export default router
